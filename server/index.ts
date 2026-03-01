d7415e2ca00b1e127dcf94cae2862291314a3679
royalking1011 <deepvaghasiya10111@gmail.com>
2026-03-01 16:06:02 +0530

    fix cors login

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,20 @@
 import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
+import cors from "cors";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
 import { setupAuth } from "./auth";
 
 const app = express();
+
+app.use(cors({
+  origin: [
+	"https://localhost:3000",
+	"https://orivya.vercel.app"
+	]
+	credentials: true
+ }));
 const httpServer = createServer(app);
 
 declare module "http" {
